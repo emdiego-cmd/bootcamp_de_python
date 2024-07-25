32ed49f821d7fc05a07fbd2cdf1615517ec0f94b
emdiego-cmd <diegofer892000@hotmail.com>
2024-07-24 21:09:02 -0500

    Add files via upload
    Bootcamp python

---
+++ variables/variables.py
@@ -0,0 +1,22 @@
+nombre = "Diego"
+bienvenida = f"Hola {nombre} feliz noche"
+
+print(bienvenida)
+print("hola" in bienvenida)
+print("Betty" not in bienvenida)
+print("Hola" not in bienvenida)
+
+#CamelCase
+nombreCompletoDeTuProfe = "Jheyson Galvis"
+
+#SnakeCase es la que debemos utilizar en Python
+nombre_completo_de_tu_profe = "Jheyson Galvis"
+
+
+
+
+
+
+
+
+
